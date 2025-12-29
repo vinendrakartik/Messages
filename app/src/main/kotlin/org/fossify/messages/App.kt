2708c9833c73dcf008a678226b26c2319d467db7
vinendrakartik <41591008+vinendrakartik@users.noreply.github.com>
2025-12-29 19:37:29 +0530

    Remove CustomCustomizationActivity and related layout
    Deleted CustomCustomizationActivity and its layout file, and removed related entries from AndroidManifest.xml. Also cleaned up App.kt by removing lifecycle callback logic for hiding the 'Apply to all' option. This simplifies the codebase and removes unused customization logic.

--- app/src/main/kotlin/org/fossify/messages/App.kt
+++ app/src/main/kotlin/org/fossify/messages/App.kt
@@ -1,15 +1,10 @@
 package org.fossify.messages
 
-import android.app.Activity
-import android.app.Application
 import android.database.ContentObserver
 import android.net.Uri
-import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.provider.ContactsContract
-import android.view.View
-import android.view.ViewGroup
 import org.fossify.commons.FossifyApp
 import org.fossify.commons.extensions.beGone
 import org.fossify.commons.extensions.hasPermission
@@ -33,47 +28,7 @@ class App : FossifyApp() {
                 }
             }
         }
-
-        registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
-            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
-            override fun onActivityStarted(activity: Activity) {}
-            override fun onActivityResumed(activity: Activity) {
-                if (activity.javaClass.name == "org.fossify.commons.activities.CustomizationActivity") {
-                    hideApplyToAllOption(activity)
-                }
-            }
-            override fun onActivityPaused(activity: Activity) {}
-            override fun onActivityStopped(activity: Activity) {}
-            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
-            override fun onActivityDestroyed(activity: Activity) {}
-        })
-    }
-
-    private fun hideApplyToAllOption(activity: Activity) {
-        val rootView = activity.findViewById<ViewGroup>(android.R.id.content)
-        rootView?.post {
-            findAndHideView(rootView, activity)
-        }
-    }
-
-    private fun findAndHideView(view: View?, activity: Activity) {
-        if (view is ViewGroup) {
-            for (i in 0 until view.childCount) {
-                val child = view.getChildAt(i)
-                val idName = try {
-                    activity.resources.getResourceEntryName(child.id)
-                } catch (e: Exception) {
-                    ""
-                }
-
-                if (idName == "customization_apply_to_all_holder") {
-                    child.beGone()
-                    return
-                }
-                findAndHideView(child, activity)
-            }
-        }
-    }
+   }
 
     private val contactsObserver = object : ContentObserver(Handler(Looper.getMainLooper())) {
         override fun onChange(selfChange: Boolean, uri: Uri?) {
